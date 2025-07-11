aa10a81b13eff1c739dc6933f3ee67df33764fa1
Luiz Netto <luiznetto@protonmail.com>
2025-07-11 10:59:04 -0300

    feat: creates home and routes screens

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
