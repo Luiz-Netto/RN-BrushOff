d94128164b4050d3af3b51cf24d6412aaacd48f7
Luiz Netto <luiznetto@protonmail.com>
2025-07-11 10:41:15 -0300

    chore: adds Testing lib and navigation

--- android/app/src/main/java/com/brushoff/MainActivity.kt
+++ android/app/src/main/java/com/brushoff/MainActivity.kt
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+  //REACT-NATIVE-SCREENS
+import android.os.Bundle;
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -19,4 +22,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  //REACT-NATIVE-SCREENS
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
